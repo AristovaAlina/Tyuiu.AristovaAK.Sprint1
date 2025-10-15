60ef538c6a493c06081335a951ccd6617a9dc98a
AristovaAlina <alinaaristova72@gmail.com>
2025-10-15 23:01:35 +0500

    Add project files.

---
+++ Tyuiu.AristovaAK.Sprint1.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.AristovaAK.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.AristovaAK.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(10, res);
+        }
+    }
+}
