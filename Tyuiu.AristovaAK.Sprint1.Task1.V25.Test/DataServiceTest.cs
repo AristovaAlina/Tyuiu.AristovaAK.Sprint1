4fc6df5c706270cb8dd106154582ad987ccaf8c1
AristovaAlina <alinaaristova72@gmail.com>
2025-10-16 00:02:15 +0500

    Task1

---
+++ Tyuiu.AristovaAK.Sprint1.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AristovaAK.Sprint1.Task1.V25.Lib;
+
+namespace Tyuiu.AristovaAK.Sprint1.Task1.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
