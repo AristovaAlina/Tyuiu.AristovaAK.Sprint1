ef256b1c47dc6df09e483b9b2f54f517ac449748
AristovaAlina <alinaaristova72@gmail.com>
2025-10-17 05:50:13 +0500

    Task5

---
+++ Tyuiu.AristovaAK.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AristovaAK.Sprint1.Task5.V5.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнила: Аристова А. К. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнила: Аристова Алина Константиновна | СМАРТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Присвоить целой переменной первую цифру из дробной части                *");
+        Console.WriteLine("* положительного вещественного числа x.                                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x;
+        Console.WriteLine("Введите значение x:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x));
+
+        Console.ReadKey();
+    }
+}
